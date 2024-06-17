d6145964997e5bd46594dfbeb8a99fb90e6651bb
tictools <jaumevillarreal@gmail.com>
2024-06-17 19:55:21 +0200

    feat: add ts-jest + jest config + jest types in tsconfig

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
-/** @type {import('jest').Config} */
-const config = {
-  moduleFileExtensions: ["js", "ts"],
-  transform: {
-    "^.+\\.m?js$": "babel-jest",
-  },
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
 };
 
-export default config;
+// /** @type {import('jest').Config} */
+// const config = {
+//   moduleFileExtensions: ["js", "ts"],
+//   transform: {
+//     "^.+\\.m?js$": "babel-jest",
+//   },
+// };
+
+// export default config;
